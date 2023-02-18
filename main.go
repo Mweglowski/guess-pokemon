7002452439748e1763991ea2ae0828c22fb73330
Marcin Weglowski <marcin.weglowski1@gmail.com>
2023-02-18 15:44:16 +0100

    almost finished

--- main.go
+++ main.go
@@ -1,16 +1,47 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"math/rand"
-	"net/http"
 	"os"
 	"strconv"
 	"strings"
 	"time"
+	"net/http"
+	"encoding/json"
+	"io/ioutil"
 )
+
+type PokemonData struct {
+	Name string `json:"name"`
+}
+
+type PokemonsList struct {
+	PokemonData PokemonData `json:"pokemon_species"`
+}
+
+type Response struct {
+	PokemonsList []PokemonsList `json:"pokemon_entries"`
+}
+
+func FetchPokemonsData() Response {
+	url := "http://pokeapi.co/api/v2/pokedex/kanto/"
+
+	response, error := http.Get(url)
+	if error != nil {
+		fmt.Print(error.Error())
+	}
+
+	responseData, error := ioutil.ReadAll(response.Body)
+	if error != nil {
+		fmt.Print(error.Error())
+	}
+
+	var data Response
+	json.Unmarshal(responseData, &data)
+
+	return data
+}
 
 func AddSpace() {
 	var space string
@@ -23,37 +54,13 @@ func AddSpace() {
 func main() {
 	fmt.Println("Guess a Pokemon from Kanto generation!")
 
-	url := "http://pokeapi.co/api/v2/pokedex/kanto/"
-
-	// FUNCTION THAT HANDLES GET HTTP REQUEST FOR RANDOM POKEMON
-	response, error := http.Get(url)
-	if error != nil {
-		fmt.Print(error.Error())
-	}
-
-	responseData, error := ioutil.ReadAll(response.Body)
-	if error != nil {
-		fmt.Print(error.Error())
-	}
-
-	type PokemonData struct {
-		Name string `json:"name"`
-	}
-	type PokemonsList struct {
-		PokemonData PokemonData `json:"pokemon_species"`
-	}
-	type Response struct {
-		PokemonsList []PokemonsList `json:"pokemon_entries"`
-	}
-
-	var data Response
-	json.Unmarshal(responseData, &data)
+	data := FetchPokemonsData()
 
 	pokemonsListLength := len(data.PokemonsList)
 	randomNumber := rand.Intn(pokemonsListLength)
 
 	pokemonToGuess := data.PokemonsList[randomNumber].PokemonData.Name
-	fmt.Println(pokemonToGuess)
+	// fmt.Println(pokemonToGuess)
 
 	// DEFINING OUTPUT STRING - WHAT USER SEES AT THE SCREEN (UNDERSCORES)
 	var outputString string
@@ -78,7 +85,10 @@ func main() {
 	guessingStarted = false
 
 	for {
+		// MAKING TOP OF AN APPLICATION CLEAR
 		AddSpace()
+
+		// DEALING WITH NOTIFICATION ABOUT LAST GUESS
 		if guessingStarted {
 			if letterGuessedCorrectly {
 				fmt.Println("You guessed!")
@@ -86,7 +96,9 @@ func main() {
 				fmt.Println("Wrong answer!")
 			}
 		}
+
 		guessingStarted = true
+
 		fmt.Println("HEARTS [" + strconv.Itoa(hearts) + "]")
 		fmt.Println("\n" + outputString + "\n")
 
@@ -95,13 +107,10 @@ func main() {
 		fmt.Print("Type letter >>>")
 		fmt.Scan(&answer)
 
-		// TASK: handle some errors related to wrong letter or wrong char
-
 		// CHECK IF LETTER IN WORD TO GUESS
 		if strings.Index(pokemonToGuess, answer) > -1 {
 			letterGuessedCorrectly = true
-			// FUNCTION TO REPLACE UNDERSCORES WITH LETTERS IF GUESSED CORRECTLY
-			// TASK: replace it with function
+			// REPLACE UNDERSCORES WITH LETTERS IF GUESSED CORRECTLY
 			for i := 0; i < len(pokemonToGuess); i++ {
 				pokemonToGuessLetter := string(pokemonToGuess[i])
 				if answer == pokemonToGuessLetter {
